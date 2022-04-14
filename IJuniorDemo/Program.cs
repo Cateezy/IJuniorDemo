26b5fc5e739152bcd929599d849001b3587f52c9
Cateezy <des.98@mail.ru>
2022-04-15 00:49:12 +0400

    Example6

--- IJuniorDemo/Program.cs
+++ IJuniorDemo/Program.cs
@@ -10,14 +10,10 @@ namespace IJuniorDemo
             int gold = Convert.ToInt32(Console.ReadLine());
             int crystals;
             int crystalCost = 120;
-            int countOfPurchasedCrystals;
-            int fullOfPurchasedCrystals;
 
             Console.WriteLine($"Укажите кол-во кристалов для покупки. Стоимость одного кристала - {crystalCost}");
-            countOfPurchasedCrystals = Convert.ToInt32(Console.ReadLine());
-            fullOfPurchasedCrystals = crystalCost * countOfPurchasedCrystals;
-            crystals = fullOfPurchasedCrystals / crystalCost;
-            gold -= fullOfPurchasedCrystals;
+            crystals = Convert.ToInt32(Console.ReadLine());
+            gold = gold - (crystals * crystalCost);
             
             Console.WriteLine($"Кол-во кристалов - {crystals}, кол-во оставшегося золота {gold}");
         }
