c6c129e37b21f59645717d673178502e18a020fc
Cateezy <des.98@mail.ru>
2022-04-14 20:24:21 +0400

    Example 4

--- IJuniorDemo/Program.cs
+++ IJuniorDemo/Program.cs
@@ -8,8 +8,10 @@ namespace IJuniorDemo
         {
             int pictures = 52;
             int limitRowPictures = 3;
+            int fullRowsOfPictures = pictures / limitRowPictures;
+            int picturesNotInFullRow = pictures % limitRowPictures;
 
-            Console.WriteLine($"Рядов заполнено картинками: {pictures / limitRowPictures} , в неполном ряду: {pictures % limitRowPictures}");
+            Console.WriteLine($"Рядов заполнено картинками: {fullRowsOfPictures} , в неполном ряду:  {picturesNotInFullRow}");
         }
     }
 }
