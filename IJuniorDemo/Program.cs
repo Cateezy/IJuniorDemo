8d23b992cad36f7ac61719c1c8f0230c21765bd6
Cateezy <des.98@mail.ru>
2022-04-15 15:45:28 +0400

    Example

--- IJuniorDemo/Program.cs
+++ IJuniorDemo/Program.cs
@@ -6,16 +6,26 @@ namespace IJuniorDemo
     {
         static void Main(string[] args)
         {
-            int increasedNumber = 5;
-            int increasedLastNumber = 96;
-            int increaseStep = 7;
+            Random random = new Random();
+            int number = random.Next(0, 100);
+            int multiplesThree = 3;
+            int multiplesFive = 5;
+            int multiplesSum = 0;
+            int cycleCounter = 1;
+            Console.WriteLine($"Сгенерированное число: {number}");
 
-            while (increasedNumber <= increasedLastNumber)
+            while (cycleCounter <= number)
             {
-                Console.Write(increasedNumber + " ");
-                increasedNumber += increaseStep;
+                if (cycleCounter % multiplesThree == 0 || cycleCounter % multiplesFive == 0)
+                {
+                    multiplesSum += cycleCounter;
+                    Console.Write(cycleCounter + " ");
+                }
+                cycleCounter++;
             }
-            
+            Console.WriteLine();
+            Console.WriteLine($"Сумма всех чисел кратных 3 и 5: {multiplesSum}");
+
         }
     }
 }
