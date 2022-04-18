4de6d9161aed0521e5b352b755daa915697f47b2
Cateezy <des.98@mail.ru>
2022-04-18 15:30:42 +0400

    Example

--- IJuniorDemo/Program.cs
+++ IJuniorDemo/Program.cs
@@ -6,68 +6,23 @@ namespace IJuniorDemo
     {
         static void Main(string[] args)
         {
-            string firstName;
-            string lastName;
-            int consoleWidth;
-            int consoleHeight;
-            string userChoose;
-            bool cycleIsActive = true;
+            Console.WriteLine("Введите имя: ");
+            string name = Console.ReadLine();
+            Console.WriteLine("Введите символ: ");
+            char symbol = Convert.ToChar(Console.ReadLine());
 
-            while (cycleIsActive)
+            for (int i = 0; i <= name.Length + 1; i++)
             {
-                Console.WriteLine("Введите SetFirstName - задать имя; SetLastName - задать фамилию");
-                Console.WriteLine("ChangeColorFont - изменить цвет текста; ChangeWindowSize - изменить размер окна");
-                Console.WriteLine("Clear - очистить консоль, CloseProgramm - завершить программу.");
-                userChoose = Console.ReadLine();
-                if (userChoose == "SetFirstName")
-                {
-                    Console.WriteLine("Введите имя: ");
-                    firstName = Console.ReadLine();
-                }
-
-                if (userChoose == "SetLastName")
-                {
-                    Console.WriteLine("Введите фамилию: ");
-                    lastName = Console.ReadLine();
-                }
-
-                if (userChoose == "ChangeColorFont")
-                {
-                    Console.WriteLine("Введите Red - красный, Green - зеленый, Yellow - желтый цвет шрифта");
-                    userChoose = Console.ReadLine();
-                    if (userChoose == "Red")
-                    {
-                        Console.ForegroundColor = ConsoleColor.Red;
-                    }
-                    else if (userChoose == "Green")
-                    {
-                        Console.ForegroundColor = ConsoleColor.Green;
-                    }
-                    else if (userChoose == "Yellow")
-                    {
-                        Console.ForegroundColor = ConsoleColor.Yellow;
-                    }
-                }
-
-                if (userChoose == "Clear")
-                {
-                    Console.Clear();
-                }
-
-                if (userChoose == "SetWindowSize")
-                {
-                    Console.WriteLine("Введите высоту окна: ");
-                    consoleHeight = Convert.ToInt32(Console.ReadLine());
-                    Console.WriteLine("Введите ширину окна: ");
-                    consoleWidth = Convert.ToInt32(Console.ReadLine());
-                    Console.SetWindowSize(consoleWidth, consoleHeight);
-                }
-
-                if (userChoose == "CloseProgramm")
-                {
-                    cycleIsActive = false;
-                }
+                Console.Write(symbol);
             }
+            Console.WriteLine();
+            Console.WriteLine($"{symbol}{name}{symbol}");
+
+            for (int i = 0; i <= name.Length + 1; i++)
+            {
+                Console.Write(symbol);
+            }
+            Console.ReadKey();
         }
     }
 }
