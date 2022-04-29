6a132f4bd2a464a1a17023c64c745580c752b57e
Cateezy <des.98@mail.ru>
2022-04-29 16:33:27 +0400

    arraysExample

--- IJuniorDemo/Program.cs
+++ IJuniorDemo/Program.cs
@@ -6,83 +6,39 @@ namespace IJuniorDemo
     {
         static void Main(string[] args)
         {
-            int health = 1000;
-            int enemyHealth = 1600;
-            int damage = 50;
-            int enemyDamage = 60;
-            int manaPool = 500;
-            int spiritsInvokeManaCost = 100;
-            int volleyManaCost = 200;
-            int stunManaCost = 200;
-            int stunTimer = 0;
-            bool isSpiritsActive = false;
-            int playerChoose;
+            int[,] array = new int[5, 5];
+            int productOfFirstColumn = 1;
+            int sumOfSecondLine = 0;
+            Random random = new Random();
 
-            Console.WriteLine("Вы герой класса: Лучник");
-
-            while (health > 0 && enemyHealth > 0)
+            for (int i = 0; i < array.GetLength(0); i++)
             {
 
-                if (manaPool > 0)
+                for (int j = 0; j < array.GetLength(1); j++)
                 {
-                    Console.WriteLine($"Введите 1, чтобы призвать духов которые повысят урон на 20 и позволят использовать Залп, маны потратится: {spiritsInvokeManaCost}");
-                    Console.WriteLine($"Введите 2, чтобы сделать Залп совместно с духами, который нанесет 350 урона, маны потратится: {volleyManaCost}");
-                    Console.WriteLine($"Введите 3, чтобы застанить босса на 2 удара, маны потратится: {stunManaCost}");
-                    Console.WriteLine($"Отавшаяся мана: {manaPool}");
-                    playerChoose = Convert.ToInt32(Console.ReadLine());
-
-                    if (playerChoose == 1 && isSpiritsActive == false)
-                    {
-                        manaPool -= spiritsInvokeManaCost;
-                        isSpiritsActive = true;
-                        damage += 20;
-                    }
-                    else if (playerChoose == 1 && isSpiritsActive == true)
-                    {
-                        Console.WriteLine("Духи уже активны!");
-                    }
-
-                    if (isSpiritsActive == true && playerChoose == 2)
-                    {
-                        manaPool -= volleyManaCost;
-                        enemyHealth -= 350;
-                    }
-                    else if (isSpiritsActive == false && playerChoose == 2)
-                    {
-                        Console.WriteLine("Духи не активны, невозможно использовать заклинание");
-                    }
-
-                    if (playerChoose == 3)
-                    {
-                        manaPool -= stunManaCost;
-                        stunTimer = 2;
-                    }
-
+                    array[i, j] = random.Next(0, 10);
+                    Console.Write(array[i, j] + " ");
                 }
 
-                if (stunTimer == 0)
-                {
-                    health -= enemyDamage;
-                }
-                else
-                {
-                    stunTimer--;
-                }
-
-                enemyHealth -= damage;
-                Console.WriteLine($"Здоровье героя: {health}\nЗдоровье босса: {enemyHealth}");
+                Console.WriteLine();
 
             }
 
-            if (health <= 0)
+            for (int i = 0; i < array.GetLength(0); i++)
             {
-                Console.WriteLine("Победил босс!");
+                int temp = array[i, 0];
+                productOfFirstColumn *= temp;
             }
 
-            if (enemyHealth <= 0)
+            Console.WriteLine($"Произведение первого столбца: {productOfFirstColumn}");
+
+            for (int i = 0; i < array.GetLength(1); i++)
             {
-                Console.WriteLine("Победил герой!");
+                int temp = array[1, i];
+                sumOfSecondLine += temp;
             }
+
+            Console.WriteLine($"Сумма второй строки: {sumOfSecondLine}");
         }
     }
 }
