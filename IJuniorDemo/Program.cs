37b04a073ac0b19cdbe31ddf1bd38459a5d72fbb
Cateezy <des.98@mail.ru>
2022-05-06 21:59:05 +0400

    Arrays

--- IJuniorDemo/Program.cs
+++ IJuniorDemo/Program.cs
@@ -7,6 +7,78 @@ namespace IJuniorDemo
         static void Main(string[] args)
         {
             Random random = new Random();
+
+            //int numbersInArray = 5;
+            //int[] array2 = new int[numbersInArray];
+            //int[] tempArray;
+            //int tempNumber;
+            //bool succesInput;
+            //int sum;
+            //string userInput;
+
+            //Console.WriteLine($"Введите {numbersInArray} чисел");
+
+            //for (int i = 0; i < array2.Length; i++)
+            //{
+            //    userInput = Console.ReadLine();
+            //    succesInput = int.TryParse(userInput, out array2[i]);
+
+            //    while (!succesInput)
+            //    {
+            //        Console.WriteLine("Ошибка ввода, введите число");
+            //        userInput = Console.ReadLine();
+            //        succesInput = int.TryParse(userInput, out array2[i]);
+            //    }
+
+            //}
+
+            //for (int i = 0; i < array2.Length; i++)
+            //{
+            //    Console.Write($"{array2[i]} ");
+            //}
+
+            //Console.WriteLine();
+
+            //while (true)
+            //{
+            //    Console.WriteLine("Введите число чтобы добавить его в массив.\nВведите exit чтобы завершить программу.\nВведите sum чтобы получить сумму всех чисел.");
+            //    userInput = Console.ReadLine();
+            //    succesInput = int.TryParse(userInput, out tempNumber);
+
+            //    if (succesInput)
+            //    {
+            //        tempArray = new int[array2.Length + 1];
+
+            //        for (int i = 0; i < array2.Length; i++)
+            //        {
+            //            tempArray[i] = array2[i];
+            //        }
+
+            //        tempArray[tempArray.Length - 1] = tempNumber;
+            //        array2 = tempArray;
+
+            //    }
+
+            //    if (userInput == "exit")
+            //    {
+            //        break;
+            //    }
+
+            //    if (userInput == "sum")
+            //    {
+            //        sum = 0;
+
+            //        for (int i = 0; i < array2.Length; i++)
+            //        {
+            //            sum += array2[i]; 
+            //        }
+
+            //        Console.WriteLine(sum);
+
+            //    }
+
+            //}
+
             int numbersInArray = 30;
             int[] array = new int[numbersInArray];
             int firstRandomNumber = 0;
@@ -32,17 +104,12 @@ namespace IJuniorDemo
                 Console.WriteLine($"Локальный максимум: {array[numbersInArray - 1]}");
             }
 
-            for (int i = 0; i <= array.Length; i++)
+            for (int i = 1; i < array.Length - 1; i++)
             {
 
-                if (i > 1 && i < numbersInArray)
+                if (array[i] > array[i - 1] && array[i] > array[i + 1])
                 {
-
-                    if (array[i] > array[i - 1] && array[i] > array[i + 1])
-                    {
-                        Console.WriteLine($"Локальный максимум: {array[i]}");
-                    }
-
+                    Console.WriteLine($"Локальный максимум: {array[i]}");
                 }
 
             }
