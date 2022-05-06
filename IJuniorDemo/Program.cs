f835651e66fc5031ebd3de56e4830395457feefa
Cateezy <des.98@mail.ru>
2022-05-06 22:48:21 +0400

    arrays

--- IJuniorDemo/Program.cs
+++ IJuniorDemo/Program.cs
@@ -7,41 +7,70 @@ namespace IJuniorDemo
         static void Main(string[] args)
         {
             Random random = new Random();
-            int numbersInArray = 30;
-            int[] array = new int[numbersInArray];
-            int firstRandomNumber = 0;
-            int lastRandomNumber = 100;
+            int numbersInArray = 5;
+            int[] array2 = new int[numbersInArray];
+            int[] tempArray;
+            int tempNumber;
+            bool succesInput;
+            int sum;
+            string userInput;
 
-            Console.WriteLine("Исходный массив: ");
+            Console.WriteLine($"Введите {numbersInArray} чисел");
 
-            for (int i = 0; i < array.Length; i++)
+            for (int i = 0; i < array2.Length; i++)
             {
-                array[i] = random.Next(firstRandomNumber, lastRandomNumber);
-                Console.Write($"{array[i]} ");
+                userInput = Console.ReadLine();
+                succesInput = int.TryParse(userInput, out array2[i]);
+
+                while (!succesInput)
+                {
+                    Console.WriteLine("Ошибка ввода, введите число");
+                    userInput = Console.ReadLine();
+                    succesInput = int.TryParse(userInput, out array2[i]);
+                }
+            }
+
+            for (int i = 0; i < array2.Length; i++)
+            {
+                Console.Write($"{array2[i]} ");
             }
 
             Console.WriteLine();
 
-            if (array[0] > array[1])
+            while (true)
             {
-                Console.WriteLine($"Локальный максимум: {array[0]}");
-            }
+                Console.WriteLine("Введите число чтобы добавить его в массив.\nВведите exit чтобы завершить программу.\nВведите sum чтобы получить сумму всех чисел.");
+                userInput = Console.ReadLine();
+                succesInput = int.TryParse(userInput, out tempNumber);
 
-            if (array[numbersInArray - 1] > array[numbersInArray - 2])
-            {
-                Console.WriteLine($"Локальный максимум: {array[numbersInArray - 1]}");
-            }
-
-            for (int i = 1; i < array.Length - 1; i++)
-            {
-
-                if (array[i] > array[i - 1] && array[i] > array[i + 1])
+                if (succesInput)
                 {
-                    Console.WriteLine($"Локальный максимум: {array[i]}");
+                    tempArray = new int[array2.Length + 1];
+
+                    for (int i = 0; i < array2.Length; i++)
+                    {
+                        tempArray[i] = array2[i];
+                    }
+                    tempArray[tempArray.Length - 1] = tempNumber;
+                    array2 = tempArray;
                 }
 
-            }
+                if (userInput == "exit")
+                {
+                    break;
+                }
 
+                if (userInput == "sum")
+                {
+                    sum = 0;
+
+                    for (int i = 0; i < array2.Length; i++)
+                    {
+                        sum += array2[i];
+                    }
+                    Console.WriteLine(sum);
+                }
+            }
         }
     }
 }
