04685c0f14f97d840556d3f5efa1a625eb75ae16
Cateezy <des.98@mail.ru>
2022-05-26 12:35:54 +0400

    stringArraySplit

--- IJuniorDemo/Program.cs
+++ IJuniorDemo/Program.cs
@@ -6,39 +6,12 @@ namespace IJuniorDemo
     {
         static void Main(string[] args)
         {
-            int numbersInArray = 30;
-            int[] array = new int[numbersInArray];
-            Random random = new Random();
-            int minNumberForRandom = 0;
-            int maxNumberForRandom = 10;
-            Console.WriteLine("Изначальный массив: ");
+            string text = "One Two Three Four";
+            string[] textArray = text.Split(' ');
 
-            for (int i = 0; i < array.Length; i++)
+            for (int i = 0; i < textArray.Length; i++)
             {
-                array[i] = random.Next(minNumberForRandom, maxNumberForRandom);
-                Console.Write($"{array[i]} ");
-            }
-
-            for (int i = 0; i < array.Length; i++)
-            {
-                for (int j = i + 1; j < array.Length; j++)
-                {
-                    int temp;
-
-                    if (array[i] > array[j])
-                    {
-                        temp = array[i];
-                        array[i] = array[j];
-                        array[j] = temp;
-                    }
-                }
-            }
-
-            Console.WriteLine("\nОтсортированный массив: ");
-
-            for (int i = 0; i < array.Length; i++)
-            {
-                Console.Write($"{array[i]} ");
+                Console.WriteLine($"{textArray[i]} ");
             }
         }
     }
