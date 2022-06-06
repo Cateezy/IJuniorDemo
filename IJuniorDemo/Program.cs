5f27f41df89be2e659c7af4acb6fffe7b3a759fb
Cateezy <des.98@mail.ru>
2022-06-06 16:31:36 +0400

    shuffleFisherYeits

--- IJuniorDemo/Program.cs
+++ IJuniorDemo/Program.cs
@@ -7,104 +7,28 @@ namespace IJuniorDemo
     {
         static void Main(string[] args)
         {
-            int playerPositionX;
-            int playerPositionY;
-            int directionX = 0;
-            int directionY = 0;
-            bool isPlaying = true;
-            char[,] map = ReadMap("map1.txt", out playerPositionX, out playerPositionY);
-            DrawMap(map);
-            DrawPlayer(ref playerPositionX, ref playerPositionY);
-            Console.CursorVisible = false;
-            
-            while (isPlaying)
+            int[] array = new int[] {1, 2, 3, 4, 5};
+
+            Shuffle(array);
+
+            foreach (var i in array)
             {
-                GetDirection(ref directionX, ref directionY);
-                ChangePlayerPosition(map, ref playerPositionX, ref playerPositionY, ref directionX, ref directionY);
+                Console.Write($"{i} ");
             }
         }
 
-        static char[,] ReadMap(string path, out int playerPositionX, out int playerPositionY)
+        static void Shuffle(int[] array)
         {
-            playerPositionX = 0;
-            playerPositionY = 0;
-            string[] tempMap = File.ReadAllLines(path);
-            char[,] map = new char[tempMap.Length, tempMap[0].Length];
+            Random random = new Random();
+            int index;
+            int temp;
 
-            for (int i = 0; i < map.GetLength(0); i++)
+            for (int i = array.Length - 1; i >= 1; i--)
             {
-                for (int j = 0; j < map.GetLength(1); j++)
-                {
-                    map[i, j] = tempMap[i][j];
-
-                    if (map[i, j] == '@')
-                    {
-                        playerPositionX = i;
-                        playerPositionY = j;
-                    }
-                }
-            }
-
-            return map;
-        }
-
-        static void DrawMap(char[,] map)
-        {
-            for (int i = 0; i < map.GetLength(0); i++)
-            {
-                for (int j = 0; j < map.GetLength(1); j++)
-                {
-                    Console.Write(map[i, j]);
-                }
-                Console.WriteLine();
-            }
-        }
-
-        static void DrawPlayer(ref int playerPositionX, ref int playerPositionY)
-        {
-            Console.SetCursorPosition(playerPositionY, playerPositionX);
-            Console.Write('@');
-        }
-
-        static void GetDirection(ref int directionX, ref int directionY)
-        {
-            if (Console.KeyAvailable)
-            {
-                ConsoleKeyInfo key = Console.ReadKey(true);
-
-                switch (key.Key)
-                {
-                    case ConsoleKey.W:
-                        directionX = -1;
-                        directionY = 0;
-                        break;
-                    case ConsoleKey.S:
-                        directionX = 1;
-                        directionY = 0;
-                        break;
-                    case ConsoleKey.A:
-                        directionX = 0;
-                        directionY = -1;
-                        break;
-                    case ConsoleKey.D:
-                        directionX = 0;
-                        directionY = 1;
-                        break;
-                }
-            }
-        }
-
-        static void ChangePlayerPosition(char[,] map, ref int playerPositionX, ref int playerPositionY, ref int directionX, ref int directionY)
-        {
-            if (map[playerPositionX + directionX, playerPositionY + directionY] != '#')
-            {
-                Console.SetCursorPosition(playerPositionY, playerPositionX);
-                Console.Write(" ");
-
-                playerPositionX += directionX;
-                playerPositionY += directionY;
-
-                DrawPlayer(ref playerPositionX, ref playerPositionY);
+                index = random.Next(i + 1);
+                temp = array[index];
+                array[index] = array[i];
+                array[i] = temp;
             }
         }
     }
