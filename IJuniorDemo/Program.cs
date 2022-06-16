a474a56130bc01ed4532743f9c651f32b2988aac
Cateezy <des.98@mail.ru>
2022-06-16 16:31:32 +0400

    list

--- IJuniorDemo/Program.cs
+++ IJuniorDemo/Program.cs
@@ -9,41 +9,42 @@ namespace IJuniorDemo
         {
             List<int> numbers = new List<int>();
             string userInput = "";
-            bool isInt;
-            int sum = 0;
 
             while(userInput != "exit")
             {
-                Console.WriteLine("Введите add, чтобы добавить число. Введите sum, чтобы просуммировать все числа в коллекции. Введите exit, чтобы завершить программу.");
+                Console.WriteLine("Введите число чтобы добавить его в коллекцию. Введите sum, чтобы просуммировать все числа в коллекции.");
+                Console.WriteLine("Введите exit, чтобы завершить программу.");
                 userInput = Console.ReadLine().ToLower();
 
-                if (userInput == "add")
-                {
-                    Console.WriteLine("Введите число: ");
-                    userInput = Console.ReadLine();
-                    isInt = int.TryParse(userInput, out int result);
-
-                    while (isInt != true)
-                    {
-                        Console.WriteLine("Вы ввели не число! Повторите ввод: ");
-                        userInput = Console.ReadLine();
-                        isInt = int.TryParse(userInput, out result);
-                    }
-
-                    numbers.Add(result);
-                }
+                IsInt(userInput, ref numbers);
 
                 if (userInput == "sum")
                 {
-
-                    foreach (var number in numbers)
-                    {
-                        sum += number;
-                    }
-
-                    Console.WriteLine($"Сумма чисел в коллекции:{sum}");
+                    SumNumbers(ref numbers);
                 }
             }
+        }
+
+        static void IsInt(string userInput, ref List<int> numbers)
+        {
+            bool isInt = int.TryParse(userInput, out int result);
+
+            if (isInt == true)
+            {
+                numbers.Add(result);
+            }
+        }
+
+        static void SumNumbers(ref List<int> numbers)
+        {
+            int sum = 0;
+
+            foreach (var number in numbers)
+            {
+                sum += number;
+            }
+
+            Console.WriteLine($"Сумма чисел в коллекции:{sum}");
         }
     }
 }
