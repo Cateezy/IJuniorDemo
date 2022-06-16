f54163f2a171e7512eeb6f1eca70496fbbf6233b
Cateezy <des.98@mail.ru>
2022-06-16 12:02:01 +0400

    queue

--- IJuniorDemo/Program.cs
+++ IJuniorDemo/Program.cs
@@ -8,9 +8,7 @@ namespace IJuniorDemo
         static void Main(string[] args)
         {
             int cashAccount = 0;
-            int clientCount = 1;
-            int userInput;
-            List<int> price = new List<int>() { 100, 200, 300 };
+            int clientNumber = 1;
             Queue<int> money = new Queue<int>();
             money.Enqueue(300);
             money.Enqueue(500);
@@ -18,24 +16,9 @@ namespace IJuniorDemo
 
             while (money.Count != 0)
             {
-                Console.WriteLine($"Обслуживается клиент №{clientCount}, кол-во денег:{money.Peek()}");
-                Console.WriteLine($"Введите 1 для покупки товара №1 с ценой {price[0]}, Введите 2 для покупки товара №2 с ценой {price[1]}");
-                Console.WriteLine($"Введите 3 для покупки товара №3 с ценой {price[2]}");
-                userInput = Convert.ToInt32(Console.ReadLine());
-
-                switch (userInput)
-                {
-                    case 1:
-                        cashAccount += price[0];
-                        break;
-                    case 2:
-                        cashAccount += price[1];
-                        break;
-                    case 3:
-                        cashAccount += price[2];
-                        break;
-                }
-
+                Console.WriteLine($"Обслуживается клиент №{clientNumber}, сумма:{money.Peek()}");
+                cashAccount += money.Peek();
+                clientNumber++;
                 Console.WriteLine($"Счет - {cashAccount}");
                 money.Dequeue();
                 Console.ReadKey();
