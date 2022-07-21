093e942e3ee81c15cada362d2002067caa74d25b
Cateezy <des.98@mail.ru>
2022-07-21 16:13:00 +0400

    ArrayToList

--- IJuniorDemo/Program.cs
+++ IJuniorDemo/Program.cs
@@ -15,19 +15,11 @@ namespace IJuniorDemo
             ConvertArrayToList(secondArray, list);
 
             Console.WriteLine("Исходный список: ");
-
-            foreach (int number in list)
-            {
-                Console.Write($"{number} ");
-            }
-
+            PrintList(list);
             RemoveDuplicateNumbers(list);
-            Console.WriteLine("\nОтформатированный список: ");
 
-            foreach (int number in list)
-            {
-                Console.Write($"{number} ");
-            }
+            Console.WriteLine("\nОтформатированный список: ");
+            PrintList(list);
         }
 
         static void ConvertArrayToList(int[] array, List<int> list)
@@ -40,7 +32,7 @@ namespace IJuniorDemo
 
         static void RemoveDuplicateNumbers(List<int> list)
         {
-            list.Sort();
+            BubbleSort(list);
 
             for (int i = 1; i < list.Count; i++)
             {
@@ -48,6 +40,32 @@ namespace IJuniorDemo
                 {
                     list.RemoveAt(i);
                 }
+            }
+        }
+
+        static void BubbleSort(List<int> list)
+        {
+            int temp;
+
+            for (int i = 0; i < list.Count; i++)
+            {
+                for (int j = 0; j < list.Count - 1; j++)
+                {
+                    if (list[j] > list[j + 1])
+                    {
+                        temp = list[j + 1];
+                        list[j + 1] = list[j];
+                        list[j] = temp;
+                    }
+                }
+            }
+        }
+
+        static void PrintList(List<int> list)
+        {
+            foreach (int number in list)
+            {
+                Console.Write($"{number} ");
             }
         }
     }
